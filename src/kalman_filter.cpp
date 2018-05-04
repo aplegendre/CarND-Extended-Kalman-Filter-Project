c2ade927f1ff3d63567f1ca12a281756a60bdedf
aplegendre <aplegendre@gmail.com>
2018-05-04 01:36:03 -0400

    First commit of project solution

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -21,22 +21,70 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+	// 	code taken from Udacity EKF lesson solutions
+	x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
-  TODO:
     * update the state by using Kalman Filter equations
+	* code taken from Udacity EKF lesson solutions
   */
+	VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
-  TODO:
     * update the state by using Extended Kalman Filter equations
   */
+	float px = x_[0];
+	float py = x_[1];
+	float vx = x_[2];
+	float vy = x_[3];
+	
+	float rho = z[0];
+	float phi = z[1];
+	float rho_dot = z[2];
+	
+	float rho_pred = sqrt(px*px + py*py);
+
+	VectorXd y(3);
+	y << rho - rho_pred, 
+		 phi - atan2(py, px), 
+		 rho_dot - (px*vx + py * vy) / rho_pred;
+
+	while ((y[1] > M_PI) || (y[1] < -M_PI)) {
+		if (y[1] > 0) {
+			y(1) = y[1] - 2 * M_PI;
+		}
+		else {
+			y(1) = y[1] - 2 * M_PI;
+		}
+	}
+
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -11,15 +11,55 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+	// taken from Udacity EKF solutions
+	VectorXd rmse(4);
+	rmse << 0, 0, 0, 0;
+
+
+	// check the validity of the following inputs:
+	//  * the estimation vector size should not be zero
+	//  * the estimation vector size should equal ground truth vector size
+	if (estimations.size() == 0 || estimations.size() != ground_truth.size()) {
+		cout << "Error: Invalid Data" << endl;
+		return rmse;
+	}
+
+	VectorXd residuals;
+	//accumulate squared residuals
+	for (int i = 0; i < estimations.size(); ++i) {
+		residuals = estimations[i] - ground_truth[i];
+		residuals = residuals.array()*residuals.array();
+		rmse += residuals;
+	}
+	//calculate the mean
+	rmse /= estimations.size();
+	//calculate the squared root
+	rmse = rmse.array().sqrt();
+
+	//return the result
+	return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+	// taken from Udacity EKF lesson solutions
+	MatrixXd Hj(3, 4);
+	//recover state parameters
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	//check division by zero
+	if (px == 0 && py == 0) {
+		cout << "Division by zero error" << endl;
+		return Hj;
+	}
+	float c1 = px * px + py * py;
+	float c2 = sqrt(px*px + py * py);
+	float c3 = c1 * c2;
+	//compute the Jacobian matrix
+	Hj << px / c2, py / c2, 0, 0,
+		-py / c1, px / c1, 0, 0,
+		py*(vx*py - vy * px) / c3, px*(vy*px - vx * py) / c3, px / c2, py / c2;
+	return Hj;
 }
